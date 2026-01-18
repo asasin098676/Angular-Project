a779a51a15ad33b9bee11105f2b6c16ebb79f8a6
Roman Onyshko <asasin09867@gmail.com>
2026-01-18 17:18:09 +0100

    add redirect when login sucses

--- src/app/components/loginPage/login.ts
+++ src/app/components/loginPage/login.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { UiInputComponent } from './input/ui-input';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -13,12 +14,19 @@ export class LoginPage {
   login = signal('');
   password = signal('');
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   async submit() {
     console.log(this.login(), this.password());
 
     if (!this.login().trim() || !this.password()) return;
+
     this.authService.setData(this.login(), this.password());
+
     await this.authService.sentLoginData();
+
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['']);
+    }
   }
 }
